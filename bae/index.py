b90d3537880c6d3a9540aefcb05521b4008635d4
xuanqinanhai <xuanqinanhai@gmail.com>
2013-08-15 10:47:59 +0800

    添加新的请求方式，支持直接URL传入

--- bae/index.py
+++ bae/index.py
@@ -1,45 +1,62 @@
-#-*- coding:utf-8 -*-
-
-from flask import Flask, g, request, render_template
-from bs4 import BeautifulSoup
-
+# encoding: UTF-8
+from flask import Flask,url_for,request,render_template
+from werkzeug.routing import BaseConverter
 app = Flask(__name__)
-app.debug = True
- 
-@app.route('/')
-def hello():
-    return render_template('index.html')
 
-@app.route('/baidu/<id>/<uk>')
-def baidu(id,uk):
-	import urllib,urllib2
-	from flask import jsonify
-	app.logger.warning('Request id:'+id)
-	app.logger.warning('Request uk:'+uk)
-	url = 'http://pan.baidu.com/share/link?shareid=%s&uk=%s' %(id,uk)
-	header = {
-		'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.8.1.14) Gecko/20080404 (FoxPlus) Firefox/2.0.0.14',
-	}
-	request = urllib2.Request(url= url,headers = header)
-	html_code = urllib2.urlopen(request).read()
+@app.route('/')
+def hello_world():
+	return render_template('index.html')
+
+def getDownloadLink(url):
 	import re
-	match = re.search(r'dlink\\.+?(http.+?)\\"',html_code,re.MULTILINE)
+	from flask import jsonify
+	match = re.search(r'http:\/\/pan.baidu.com\/share\/link\?shareid=(\d+)&uk=(\d+)',url)
 	if(match):
-		return jsonify(
+		import urllib,urllib2
+		id = match.group(1)
+		uk = match.group(2)
+		header = {
+			'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.8.1.14) Gecko/20080404 (FoxPlus) Firefox/2.0.0.14'
+		}
+		request = urllib2.Request(url = url, headers = header)
+		html_code = urllib2.urlopen(request).read()
+		match = re.search(r'dlink\\.+?(http.+?)\\"',html_code,re.MULTILINE)
+		if(match):
+			return jsonify(
+					url = url,
+					id=id,
+					uk=uk,
+					error=False,
+					link=match.group(1),
+					type='baidu'
+				)
+		else:
+			return jsonify(
+				url = url,
 				id=id,
 				uk=uk,
-				error=False,
-				link=match.group(1),
+				error=True,
 				type='baidu'
 			)
 	else:
 		return jsonify(
-			id=id,
-			uk=uk,
-			error=True,
-			type='baidu'
-		)
+				url = url,
+				error = True,
+				type = 'baidu',
+			)
 
+@app.route('/baidu/')
+def baiduArgs():
+	url = request.args['url'];
+	if 'uk=' in url:
+		return getDownloadLink(url)
+	else:
+		uk = request.args['uk']
+		return getDownloadLink(url+ '&uk=' + uk)
+
+@app.route('/baidu/<id>/<uk>')
+def baidu(id,uk):
+	return getDownloadLink('http://pan.baidu.com/share/link?shareid=%s&uk=%s' % (id , uk))
  
 from bae.core.wsgi import WSGIApplication
 application = WSGIApplication(app)
